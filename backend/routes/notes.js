fbdb666d122bcb9d6bda9b0a9b4185ac326ae177
Satish Parmar <Satish Parmar1>
2023-08-15 22:36:20 +0530

    day22

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    const obj = {
-     
-    };
-    res.json([]);
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const note = new Note({
+            title,
+            description,
+            tag,
+            user: req.user.id
+        });
+
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: 'Server Error' }); 
+    }
 });
 
 module.exports = router;
